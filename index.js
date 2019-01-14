abae44ce3685e1ded7576cdbc42293682cd64011
Andrew Parrucci <am.parrucci@gmail.com>
2019-01-14 23:21:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+const pickup = 42;
+function distanceFromHqInBlocks (dropoff) {
+return dropoff - pickup;
+  }
+  
+  distanceFromHqInBlocks (43);
+  distanceFromHqInBlocks (50);
+  Math.abs(distanceFromHqInBlocks (34));
+  
+
+function distanceFromHqInFeet (x) {
+return (x - pickup) * 264;
+  }
+ distanceFromHqInFeet (50);
+ 
+ 
+
+function calculatesFarePrice (fare) {
+  
+}
