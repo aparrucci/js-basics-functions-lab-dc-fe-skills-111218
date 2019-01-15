c5556232bf782321b45d21e2021b3737978c7fa6
Andrew Parrucci <am.parrucci@gmail.com>
2019-01-15 00:52:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,10 +31,13 @@ function calculatesFarePrice (start, destination) {
 let feet = distanceTravelledInFeet (start, destination);
 if (feet <=400)
  {
-  return `gives customers a free sample`;
+  return 0;
 } 
-if (feet >400, feet <2500) {
+if (feet >400, feet <2000) {
   return (feet - 400) * 0.02; 
+}
+if (feet >2000, feet<2500) {
+  return 25
 }
 
 if (feet >2500) {
